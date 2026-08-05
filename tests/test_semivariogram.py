6a88c2d4d21b4e8c61a4215d4102d32a864ab5f2
ChelseaM28 <cvmomoh@ucdavis.edu>
2026-08-05 21:40:30 +0000

    testing test files

---
+++ tests/test_semivariogram.py
@@ -0,0 +1,12 @@
+# Filename: test_semivariogram.py
+# Author : Chelsea Momoh
+# Date : 2026-08-05
+# Version : 1.0
+# Description : Test script for empirical semivariogram.
+# * - * - * - * - * - * - * - * - * - * - * - * - * - * - * -
+
+
+def test_print_statement():
+    test_finished = "Yankee Doodle Went to Town. "
+    assert test_finished
+
